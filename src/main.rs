9caaad82398eeb37d8195a365f563fb69ad3a294
Surma <surma@surma.dev>
2022-07-23 23:20:11 +0100

    Transform parser working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
+use std::env;
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
+use std::process;
 
 mod ast;
 mod linker;
@@ -31,7 +33,7 @@ struct Args {
     #[clap(
         short = 'c',
         long = "emit-binary",
-        default_value_t = true,
+        default_value_t = false,
         value_parser
     )]
     emit_binary: bool,
@@ -46,16 +48,21 @@ struct Args {
     wat2wasm_flags: Option<String>,
 
     /// Comma-separated list of transforms.
-    #[clap(long = "transform", arg_enum, value_parser=transform_list_parser, name="TRANSFORM")]
-    transform_list: Vec<passes::Pass>,
+    #[clap(
+        long = "transform",
+        name = "TRANSFORMS",
+        default_value = "import, sort"
+    )]
+    transform_list: String,
 
     /// Root for import path resolution.
     #[clap(short = 'r', long = "root", value_parser)]
     root: Option<String>,
 }
 
-fn transform_list_parser(s: &str) -> std::result::Result<Vec<passes::Pass>, String> {
-    let list: Vec<std::result::Result<passes::Pass, String>> = s
+fn transform_list_parser(args: &Args) -> std::result::Result<Vec<passes::Pass>, String> {
+    let list: Vec<std::result::Result<passes::Pass, String>> = args
+        .transform_list
         .split(",")
         .map(|item| {
             let name = item.trim();
@@ -67,38 +74,57 @@ fn transform_list_parser(s: &str) -> std::result::Result<Vec<passes::Pass>, Stri
         })
         .collect();
 
-    Result::from_iter(list)
+    let result: Vec<passes::Pass> = Result::from_iter(list)?;
+    Ok(result)
 }
 
 fn main() {
     let args = Args::parse();
 
-    let root = args.root.unwrap_or_else(|| {
-        std::env::current_dir()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .to_string()
-    });
+    let transform_list = transform_list_parser(&args).unwrap();
+
+    let root = args
+        .root
+        .unwrap_or_else(|| env::current_dir().unwrap().to_str().unwrap().to_string());
 
     let loader = loader::FileSystemLoader::new(root);
     let mut linker = linker::Linker::new(Box::new(loader));
-    for pass in &args.transform_list {
-        linker.passes.push(*pass);
+    for pass in transform_list.into_iter() {
+        linker.passes.push(pass);
     }
 
     let module = if args.input == "-" {
         let mut content = String::new();
-        std::io::stdin().read_to_string(&mut content).unwrap();
+        io::stdin().read_to_string(&mut content).unwrap();
         linker.link_raw(content).unwrap()
     } else {
         linker.link_file(&args.input).unwrap()
     };
+    let serialized_module = format!("{}", module);
+    println!("{}", serialized_module);
 
     let mut output: Box<dyn Write> = if args.output == "-" {
-        Box::new(std::io::stdout())
+        Box::new(io::stdout())
     } else {
         Box::new(File::create(args.output).unwrap())
     };
-    write!(output, "{}", module).unwrap();
+    if args.emit_binary {
+        let mut child = process::Command::new("wat2wasm")
+            // .args(["-o", "-"])
+            .arg("-")
+            .stdin(process::Stdio::piped())
+            .stdout(process::Stdio::piped())
+            .stderr(process::Stdio::inherit())
+            .spawn()
+            .unwrap();
+        child
+            .stdin
+            .take()
+            .unwrap()
+            .write_all(serialized_module.as_bytes())
+            .unwrap();
+        io::copy(&mut child.stdout.take().unwrap(), &mut output).unwrap();
+    } else {
+        output.write_all(serialized_module.as_bytes()).unwrap();
+    }
 }
