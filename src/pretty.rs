0248756403c3d69cdd964b70d1d44065ea18b039
Surma <surma@shopify.com>
2022-09-01 23:06:53 +0100

    Better prettyprinter

--- src/pretty.rs
+++ src/pretty.rs
@@ -15,13 +15,17 @@ fn pretty_print_internal(node: &Node, mut level: usize, buffer: &mut String) {
                 *buffer += &format!("\n{}{}", "  ".repeat(level), attr);
             }
             Item::Node(node) => {
-                match node.name.as_ref() {
-                    "param" | "global.get" | "local.get" | "start" | "result" | "local"
-                    | "export" | "table" | "memory" => {
-                        *buffer += &format!("\n{}{}", "  ".repeat(level), node)
-                    }
-                    _ => pretty_print_internal(node, level, buffer),
-                };
+                if node.name.as_str().ends_with(".const") {
+                    *buffer += &format!("\n{}{}", "  ".repeat(level), node)
+                } else {
+                    match node.name.as_ref() {
+                        "param" | "global.get" | "local.get" | "start" | "result" | "local"
+                        | "export" | "table" | "memory" | "import" | "global" => {
+                            *buffer += &format!("\n{}{}", "  ".repeat(level), node)
+                        }
+                        _ => pretty_print_internal(node, level, buffer),
+                    };
+                }
             }
             Item::Nothing => {}
         }
