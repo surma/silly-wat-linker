bc68cec11d6d316d317c407d499a02682d133290
Surma <surma@surma.dev>
2022-09-06 09:49:55 +0100

    Formatter cleanup

--- src/pretty.rs
+++ src/pretty.rs
@@ -1,3 +1,5 @@
+use std::iter::Peekable;
+
 use crate::ast::{Item, Node};
 
 pub fn pretty_print(node: &Node) -> String {
@@ -33,47 +35,64 @@ fn is_function_header_item(item: &Item) -> bool {
     }
 }
 
-fn pretty_print_func_params(node: &Node, level: usize, buffer: &mut String) {
+fn item_has_node_name(v: Option<&&Item>, name: &str) -> bool {
+    v.and_then(|v| v.as_node())
+        .map(|v| v.name == name)
+        .unwrap_or(false)
+}
+
+fn item_attribute_matches_predicate<F>(v: Option<&&Item>, pred: F) -> bool
+where
+    F: Fn(&str) -> bool,
+{
+    v.and_then(|v| v.as_attribute())
+        .map(|v| pred(v))
+        .unwrap_or(false)
+}
+
+fn item_matches_predicate<F>(v: Option<&&Item>, pred: F) -> bool
+where
+    F: Fn(&Item) -> bool,
+{
+    v.map(|v| pred(v)).unwrap_or(false)
+}
+
+fn pretty_print_func(node: &Node, level: usize, buffer: &mut String) {
     assert!(node.name == "func");
+    *buffer += &format!("\n\n{}({}", "\t".repeat(level), node.name);
     let mut it = node.items.iter().peekable();
 
-    // Print function name if any
-    if it
-        .peek()
-        .and_then(|v| v.as_attribute())
-        .map(|v| v.starts_with("$"))
-        .unwrap_or(false)
-    {
+    // Print function name and export if any
+    if item_attribute_matches_predicate(it.peek(), |v| v.starts_with("$")) {
         *buffer += " ";
         *buffer += it.next().unwrap().as_attribute().unwrap();
     }
+
+    if item_has_node_name(it.peek(), "export") {
+        *buffer += &format!(" {}", it.next().unwrap());
+    }
+
     // Print function header
-    while it
-        .peek()
-        .map(|item| is_function_header_item(item))
-        .unwrap_or(false)
-    {
-        pretty_print_item(it.next().unwrap(), level, buffer)
+    while item_matches_predicate(it.peek(), |v| is_function_header_item(v)) {
+        pretty_print_item(it.next().unwrap(), level + 1, buffer)
     }
-    if it.peek().is_some() {
-        *buffer += "\n";
-    }
+
     // Print locals
-    while it
-        .peek()
-        .and_then(|item| item.as_node())
-        .map(|node| node.name == "local")
-        .unwrap_or(false)
-    {
-        pretty_print_item(it.next().unwrap(), level, buffer);
+    if item_has_node_name(it.peek(), "local") {
+        *buffer += "\n";
+        while item_has_node_name(it.peek(), "local") {
+            pretty_print_item(it.next().unwrap(), level + 1, buffer);
+        }
     }
+
+    // Print body
     if it.peek().is_some() {
         *buffer += "\n";
+        for item in it {
+            pretty_print_item(item, level + 1, buffer);
+        }
     }
-    // Print body
-    for item in it {
-        pretty_print_item(item, level, buffer);
-    }
+    *buffer += ")"
 }
 
 fn pretty_print_item(item: &Item, level: usize, buffer: &mut String) {
@@ -88,31 +107,37 @@ fn pretty_print_item(item: &Item, level: usize, buffer: &mut String) {
     }
 }
 
+fn pretty_print_call(node: &Node, level: usize, buffer: &mut String) {
+    assert!(node.name == "call");
+    *buffer += &format!("\n{}({}", "\t".repeat(level), node.name);
+    *buffer += &format!(
+        " {}",
+        node.items
+            .get(0)
+            .and_then(|item| item.as_attribute())
+            .unwrap_or("")
+    );
+    for item in node.items.iter().skip(1) {
+        pretty_print_item(item, level + 1, buffer);
+    }
+    *buffer += ")";
+}
+fn pretty_print_as_single_line(node: &Node, level: usize, buffer: &mut String) {
+    *buffer += &format!("\n{}{}", "\t".repeat(level), node);
+}
+
 fn pretty_print_node_internal(node: &Node, level: usize, buffer: &mut String) {
     if is_single_line_node_type(node) || has_at_most_one_simple_attribute(node) {
-        *buffer += &format!("\n{}{}", "\t".repeat(level), node);
-        return;
+        pretty_print_as_single_line(node, level, buffer);
     } else if node.name == "func" {
-        // Extra new-line before a function
-        *buffer += &format!("\n\n{}({}", "\t".repeat(level), node.name);
-        pretty_print_func_params(node, level + 1, buffer);
+        pretty_print_func(node, level, buffer);
     } else if node.name == "call" {
-        *buffer += &format!("\n{}({}", "\t".repeat(level), node.name);
-        *buffer += &format!(
-            " {}",
-            node.items
-                .get(0)
-                .and_then(|item| item.as_attribute())
-                .unwrap_or("")
-        );
-        for item in node.items.iter().skip(1) {
-            pretty_print_item(item, level + 1, buffer);
-        }
+        pretty_print_call(node, level, buffer);
     } else {
         *buffer += &format!("\n{}({}", "\t".repeat(level), node.name);
         for item in node.items.iter() {
             pretty_print_item(item, level + 1, buffer);
         }
+        *buffer += ")";
     }
-    *buffer += ")";
 }
